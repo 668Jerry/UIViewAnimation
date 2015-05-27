7164fee871f228bd7f20fddc6eb1e1f69d462e05
Novel <668Jerry@gmail.com>
2015-05-27 19:22:33 +0800

    with global

--- UIViewAnimation/ViewController.m
+++ UIViewAnimation/ViewController.m
@@ -19,9 +19,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self.view setBackgroundColor:[UIColor redColor]];
+    UIView *uivGlobal = [[UIView alloc]initWithFrame:CGRectMake(60, 60, 230, 230)];
+    [self.view addSubview:uivGlobal];
     UIView *uivMove = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 30, 30)];
     [uivMove setBackgroundColor:[UIColor whiteColor]];
-    [self.view addSubview:uivMove];
+    [uivGlobal addSubview:uivMove];
     [uivMove moveTo:uivMove destination:CGPointMake(300, 300) duration:5 option:UIViewAnimationOptionLayoutSubviews];
 }
 
